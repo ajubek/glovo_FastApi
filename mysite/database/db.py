4259b28cc14c402e430bac4164edfefcfebd8a78
ajubek <zyldyzbekalihanov915@gmail.com>
2026-03-08 20:14:47 +0600

    req.txt added

--- mysite/database/db.py
+++ mysite/database/db.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "postgresql://postgres:admin@db/glovo_kg_FastApi"
-
+DB_URL = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(bind=engine)
